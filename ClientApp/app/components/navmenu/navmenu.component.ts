f5203119b1efbe64512d5a5f5cf47d76b3132341
Richard Boomsma <richardboomsma@gmail.com>
2017-10-25 19:56:25 +0200

    Corrected logout code

--- ClientApp/app/components/navmenu/navmenu.component.ts
+++ ClientApp/app/components/navmenu/navmenu.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from "../security/auth.service";
 import { Router } from "@angular/router";
+import { Http } from "@angular/http";
 
 @Component({
     selector: 'nav-menu',
@@ -8,16 +9,19 @@ import { Router } from "@angular/router";
     styleUrls: ['./navmenu.component.css']
 })
 export class NavMenuComponent implements OnInit {
-    user = { name: "USER", role: "ADMIN"};
+    user = { name: "USER", role: "ADMIN" };
 
-    constructor(private authService: AuthService, private router: Router) { }
+    constructor(private authService: AuthService, private http: Http, private router: Router) { }
 
     ngOnInit(): void {
 
     }
 
     logout(): void {
-        this.authService.logout();
-        this.router.navigate(['login']);
+        this.http.post('api/account/loggoff', { headers: this.authService.contentHeaders() })
+            .subscribe(response => {
+                this.authService.logout();
+                this.router.navigate(['login']);
+            });
     }
 }
